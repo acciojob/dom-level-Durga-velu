b4600ba11b018f41b3837b016db847d8ba64108e
Durga-velu <durgadevi134babu@gmail.com>
2025-03-27 21:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+
+function display(){
+	 let level=document.getElementById("level")
+let counter=0;
+	while(level.parentNode !==null){
+		level=level.parentNode
+			counter++
+	}
+	return `The level of the element is: ${counter}`
+}
+alert(display())
+
+
+
+
+
+
+
+
+
+
+
+
+
